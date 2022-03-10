16c687e3efe844f9fc330fb24206d232785daf30
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-03-10 14:28:44 -0500

    Assertion error troubleshooting

--- music/views.py
+++ music/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
+from rest_framework import status
 
-# Create your views here.
+
+
+api_view(['GET', 'POST'])
+def songs_list(request):
+
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_detail(request, pk):
+    song = get_object_or_404(song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(Song)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
