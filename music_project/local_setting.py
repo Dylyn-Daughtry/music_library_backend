a29899c9648deab5185aa59d59a6a20a1ed8b591
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-03-10 11:35:56 -0500

    Changed database settings and created music app

---
+++ music_project/local_setting.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-c(3p%3jm2_fdg@kp^qy@pqa&n01tx*26zs8tzcl+@7w)hc7t!5'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Daughtry'
+    }
+}
